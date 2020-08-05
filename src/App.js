5dea2c548b97b0bd5434dfc8667c98b688a6da62
Thainá Ribeiro Lima <thainaribeiro96.tr@gmail.com>
2020-08-04 22:50:38 -0300

    Iniciando o projeto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+//import BannerMain from './components/BannerMain';
+
+function App() {
+  return (
+    <div>
+      <Menu/>
+
+    </div>
+  );
+}
+
+export default App;
